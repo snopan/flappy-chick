2933f05d5eb24cd1a090b6a25d985989b16cb333
snopan <snopan@protonmail.com>
2024-06-14 15:18:52 +1000

    Use animation without resources prefix

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use bevy::prelude::*;
 fn main() {
     println!("Hello, world!");
     App::new()
-        .init_resource::<resources::Animations>()
+        .init_resource::<Animations>()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_systems(Startup, setup)
         .add_systems(Update, input)
