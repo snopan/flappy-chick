7faecb7359c46b15d00ac256796017231d623a9c
snopan <snopan@protonmail.com>
2024-07-19 17:14:23 +1000

    Improve rendering by short circuiting if out of frame

--- drawable/render.go
+++ drawable/render.go
@@ -3,7 +3,9 @@ package drawable
 import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/snopan/flappy-chick/component"
+	"github.com/snopan/flappy-chick/options"
 	"github.com/yohamta/donburi"
+	"github.com/yohamta/donburi/features/math"
 	"github.com/yohamta/donburi/features/transform"
 	"github.com/yohamta/donburi/filter"
 )
@@ -25,26 +27,42 @@ func (r *Render) Draw(w donburi.World, screen *ebiten.Image) {
 			return
 		}
 
+		// Get relevant information
+		position := transform.WorldPosition(e)
+		rotation := transform.WorldRotation(e)
+		scale := transform.WorldScale(e)
 		size := sprite.Image.Bounds().Size()
 
-		// transform := transform.Transform.Get(e)
-		op := &ebiten.DrawImageOptions{}
+		if !SpriteInFrame(math.NewVec2(float64(size.X), float64(size.Y)), position, scale) {
+			return
+		}
 
 		// Center on top left so we can apply rotation and scaling
+		op := &ebiten.DrawImageOptions{}
 		op.GeoM.Translate(-float64(size.X)/2, -float64(size.Y)/2)
 
-		// Apply rotation
-		rotation := transform.WorldRotation(e)
+		// Apply all transformations
 		op.GeoM.Rotate(rotation)
-
-		// Apply scaling
-		scale := transform.WorldScale(e)
 		op.GeoM.Scale(scale.X, scale.Y)
-
-		// Then move the image to it's coordinates
-		position := transform.WorldPosition(e)
 		op.GeoM.Translate(position.X, position.Y)
 
 		screen.DrawImage(sprite.Image, op)
 	})
+}
+
+func SpriteInFrame(size, position, scale math.Vec2) bool {
+	topLeft := math.NewVec2(
+		position.X-size.X/2*scale.X,
+		position.Y-size.Y/2*scale.Y,
+	)
+
+	bottomRight := math.NewVec2(
+		position.X+size.X/2*scale.X,
+		position.Y+size.Y/2*scale.Y,
+	)
+
+	topLeftInWindow := 0.0 <= topLeft.X && topLeft.X <= options.WindowWidth && 0.0 <= topLeft.Y && topLeft.Y <= options.WindowHeight
+	bottomRightInWindow := 0.0 <= bottomRight.X && bottomRight.X <= options.WindowWidth && 0.0 <= bottomRight.Y && bottomRight.Y <= options.WindowHeight
+
+	return topLeftInWindow || bottomRightInWindow
 }
