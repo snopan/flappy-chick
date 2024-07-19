e7b8bc5112982f789961e65c97604839ecb62fe6
snopan <snopan@protonmail.com>
2024-07-19 16:08:59 +1000

    Simplified function for building pipe sections

--- entity/pipe.go
+++ entity/pipe.go
@@ -30,53 +30,27 @@ func CreatePipe(w donburi.World, centerY float64) {
 	component.SetRectangleCollider(center, 0.0, options.PipeGap, component.AnchorCenter)
 	transform.AppendChild(pipe, center, false)
 
-	CreateTopPipe(w, pipe, -options.PipeGap/2.0)
-	CreateBottomPipe(w, pipe, options.PipeGap/2.0)
+	CreatePipeSections(w, pipe, -options.PipeGap/2.0, false)
+	CreatePipeSections(w, pipe, options.PipeGap/2.0, true)
 }
 
-func CreateTopPipe(w donburi.World, pipe *donburi.Entry, startY float64) {
+func CreatePipeSections(w donburi.World, pipe *donburi.Entry, startY float64, buildBottom bool) {
 
-	// Create the parent for top pipe
-	top := w.Entry(w.Create(
+	// Create the parent for pipe section
+	sectionParent := w.Entry(w.Create(
 		component.RectangleCollider,
 		transform.Transform,
 	))
-	transform.SetWorldPosition(top, math.NewVec2(0.0, startY))
-	transform.AppendChild(pipe, top, false)
+	transform.SetWorldPosition(sectionParent, math.NewVec2(0.0, startY))
+	transform.AppendChild(pipe, sectionParent, false)
 
-	// Then start building the pipe sections
-	currentY := 0.0
-	createdEdge := false
-	pipeMiddle := sprite.GetSprite(sprite.PipeMiddle)
-	pipeSectionHeight := float64(pipeMiddle.Bounds().Size().Y)
-	for stdmath.Abs(currentY) < options.WindowHeight {
-		pipeSprite := pipeMiddle
-		if !createdEdge {
-			pipeSprite = sprite.GetSprite(sprite.PipeBottom)
-			createdEdge = true
-		}
-
-		pipeSection := w.Entry(w.Create(
-			component.Sprite,
-			transform.Transform,
-		))
-		component.SetSprite(pipeSection, pipeSprite)
-		transform.SetWorldPosition(pipeSection, math.NewVec2(0.0, currentY))
-		transform.SetWorldScale(pipeSection, math.NewVec2(options.PipeScale, options.PipeScale))
-		transform.AppendChild(top, pipeSection, false)
-		currentY -= pipeSectionHeight * options.PipeScale
+	// Define which direction the pipe should be built and what edge sprite to use
+	buildDirect := -1.0
+	edgeSprite := sprite.GetSprite(sprite.PipeBottom)
+	if buildBottom {
+		buildDirect = 1.0
+		edgeSprite = sprite.GetSprite(sprite.PipeTop)
 	}
-}
-
-func CreateBottomPipe(w donburi.World, pipe *donburi.Entry, startY float64) {
-
-	// Create the parent for bottom pipe
-	bottom := w.Entry(w.Create(
-		component.RectangleCollider,
-		transform.Transform,
-	))
-	transform.SetWorldPosition(bottom, math.NewVec2(0.0, startY))
-	transform.AppendChild(pipe, bottom, false)
 
 	// Then start building the pipe sections
 	currentY := 0.0
@@ -86,7 +60,7 @@ func CreateBottomPipe(w donburi.World, pipe *donburi.Entry, startY float64) {
 	for stdmath.Abs(currentY) < options.WindowHeight {
 		pipeSprite := pipeMiddle
 		if !createdEdge {
-			pipeSprite = sprite.GetSprite(sprite.PipeTop)
+			pipeSprite = edgeSprite
 			createdEdge = true
 		}
 
@@ -97,7 +71,7 @@ func CreateBottomPipe(w donburi.World, pipe *donburi.Entry, startY float64) {
 		component.SetSprite(pipeSection, pipeSprite)
 		transform.SetWorldPosition(pipeSection, math.NewVec2(0.0, currentY))
 		transform.SetWorldScale(pipeSection, math.NewVec2(options.PipeScale, options.PipeScale))
-		transform.AppendChild(bottom, pipeSection, false)
-		currentY += pipeSectionHeight * options.PipeScale
+		transform.AppendChild(sectionParent, pipeSection, false)
+		currentY += buildDirect * pipeSectionHeight * options.PipeScale
 	}
 }
